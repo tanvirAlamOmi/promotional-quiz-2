969ff3769ab98d46887e5ef56d7bd771c01e801e
asadurjaman <assadurjamansagor24@gmail.com>
2021-09-13 14:05:30 +0600

    quiz page asset link cdn add

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -504,12 +504,14 @@
     </script>
     <!-- preloader js end-->
     <!-- Boostrap cdn -->
-    <script src="/resources/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+    
     <!-- fontawesome js start-->
-    <script src="/resources/js/fontawesome.min.js"></script>
+    <script src="{{asset('js/fontawesome.min.js')}}"></script>
+    
     <!-- fontawesome js end-->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
+    <script src="{{asset('js\jquery-3.6.0.min.js')}}"></script>
+    
     <script>
       let boxNum = 0;
       let progressWidth = 0;
